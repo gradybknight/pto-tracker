61b73d20292d58497b5f0a6f86152b11c3cd028d
Grady Knight <grady.knight@smashingboxes.com>
2020-04-19 08:00:01 -0400

    new users get logged to users collection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,8 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+const admin = require("firebase-admin");
+
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +10,12 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const newUserSignIn = functions.auth.user().onCreate((user) => {
+  firestore.collection("users").doc(user.uid).set({
+    displayName: user.displayName,
+    email: user.email,
+    isRecognized: false,
+    isAdmin: false,
+  });
+});
